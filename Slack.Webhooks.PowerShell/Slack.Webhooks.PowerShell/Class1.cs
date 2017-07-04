e09b79bab8ef62fe4f75a1e2f0b60f4cdfb54637
KyleSmith <sniperwolf3@gmail.com>
2017-07-04 17:30:00 +1000

    Initial Commit: - Added NuGets - Added References

---
+++ Slack.Webhooks.PowerShell/Slack.Webhooks.PowerShell/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using Slack.Webhooks;
+
+namespace Slack.Webhooks.PowerShell
+{
+    public class Class1
+    {
+    }
+}
