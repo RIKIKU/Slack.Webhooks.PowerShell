e9408d50ca07478cb908394f2439eaf9164b6cf7
KyleSmith <sniperwolf3@gmail.com>
2017-07-05 17:57:52 +1000

    Added some more base stuff.

--- Slack.Webhooks.PowerShell/Slack.Webhooks.PowerShell/Class1.cs
+++ Slack.Webhooks.PowerShell/Slack.Webhooks.PowerShell/Class1.cs
@@ -49,6 +49,7 @@ namespace Slack.Webhooks.PowerShell
 
         protected override void ProcessRecord()
         {
+            WriteDebug(string.Format("Sending Message to {0}", URI));
             slackClient.Post(Message);
         }
 
@@ -57,11 +58,11 @@ namespace Slack.Webhooks.PowerShell
             base.EndProcessing();
         }
     }
-    [Cmdlet(VerbsCommon.New, "SlackMessage", SupportsShouldProcess = false)]
+    [Cmdlet(VerbsCommon.New, "SlackMessage", SupportsShouldProcess = false, SupportsTransactions = false)]
     public class CreateSlackMessage : PSCmdlet
     {
         [Parameter(
-            Mandatory = true,
+            Mandatory = false,
             Position = 0
             )]
         public string Channel
@@ -70,7 +71,7 @@ namespace Slack.Webhooks.PowerShell
             set;
         }
         [Parameter(
-            Mandatory = true,
+            Mandatory = false,
             Position = 1
             )]
         public string Text
@@ -96,22 +97,37 @@ namespace Slack.Webhooks.PowerShell
             get;
             set;
         }
-        public bool LinkNames
+        [Parameter(Mandatory = false)]
+        public SwitchParameter LinkNames
         {
             get;
             set;
         }
-        public bool Markdown
+        [Parameter(Mandatory = false)]
+        public SwitchParameter NotMarkdown
         {
             get;
             set;
         }
 
         [Parameter(
-            Mandatory = false
+            Mandatory = false,
+            HelpMessage = "https://api.slack.com/docs/message-formatting"
             )]
         [ValidateSet("full","none")]
         public string ParseMode
+        {
+            get;
+            set;
+        }
+        [Parameter(Mandatory = false)]
+        public string IconUrl
+        {
+            get;
+            set;
+        }
+        [Parameter(Mandatory = false)]
+        public SlackAttachment Attachments
         {
             get;
             set;
@@ -123,20 +139,136 @@ namespace Slack.Webhooks.PowerShell
 
         protected override void ProcessRecord()
         {
-
+            WriteDebug("Instansiating SlackMessage")
             SlackMessage message = new SlackMessage
             {
                 Channel = Channel,
                 Text = Text,
                 Username = Username,
-                LinkNames = LinkNames,
-                Mrkdwn = Markdown,
-                
-            };
-           
-            message.Parse
+                Parse = ParseMode
+        };
             
+            if(!string.IsNullOrEmpty(IconUrl))
+            {
+                WriteDebug("Building IconUrl URIBuilder");
+                UriBuilder IconURI = new UriBuilder(IconUrl);
+                message.IconUrl = IconURI.Uri;
+            }
+            if(NotMarkdown){message.Mrkdwn = false;}
+            if(LinkNames) { message.LinkNames = true; }
+            if(Attachments != null)
+            {
+                WriteDebug("Attachments Found. Converting to list");
+                message.Attachments = new List<SlackAttachment> { Attachments };
+            }
+            WriteDebug("Message Created Ouputting to pipeline");
             WriteObject(message);
+        }
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+        }
+    }
+    [Cmdlet(VerbsCommon.New, "SlackAttachment", SupportsShouldProcess = false, SupportsTransactions = false)]
+    public class CreateSlackAttachment : PSCmdlet
+    {
+        [Parameter(
+            Mandatory = false,
+            Position = 0
+            )]
+        public string Channel
+        {
+            get;
+            set;
+        }
+        [Parameter(
+            Mandatory = false,
+            Position = 1
+            )]
+        public string Text
+        {
+            get;
+            set;
+        }
+        [Parameter(
+            Mandatory = false,
+            Position = 2
+            )]
+        public string IconEmoji
+        {
+            get;
+            set;
+        }
+        [Parameter(
+            Mandatory = false,
+            Position = 3
+            )]
+        public string Username
+        {
+            get;
+            set;
+        }
+        [Parameter(Mandatory = false)]
+        public SwitchParameter LinkNames
+        {
+            get;
+            set;
+        }
+        [Parameter(Mandatory = false)]
+        public SwitchParameter NotMarkdown
+        {
+            get;
+            set;
+        }
+
+        [Parameter(
+            Mandatory = false,
+            HelpMessage = "https://api.slack.com/docs/message-formatting"
+            )]
+        [ValidateSet("full", "none")]
+        public string ParseMode
+        {
+            get;
+            set;
+        }
+        [Parameter(Mandatory = false)]
+        public string IconUrl
+        {
+            get;
+            set;
+        }
+        [Parameter(Mandatory = false)]
+        public SlackAttachment Attachments
+        {
+            get;
+            set;
+        }
+
+        protected override void BeginProcessing()
+        {
+        }
+
+        protected override void ProcessRecord()
+        {
+            WriteDebug("Instansiating SlackMessage");
+            SlackAttachment attachment = new SlackAttachment
+            {
+                AuthorIcon = "",
+                AuthorLink = "",
+                AuthorName = "",
+                Color = "",
+                Fallback = "",
+                ImageUrl = "",
+                Pretext = "",
+                Text = "",
+                ThumbUrl = "",
+                Title = "",
+                TitleLink = ""
+            };
+            //Fields
+            //MrkdwnIn
+            WriteObject(attachment);
         }
 
         protected override void EndProcessing()
